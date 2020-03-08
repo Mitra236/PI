7b421c79fa1833ec67eaad28f62aa396b382799b
Mitra Pantic <mitra196506@gmail.com>
2020-03-08 11:50:33 +0100

    Added project

---
+++ PoslovnaBanka/src/main/java/com/pi/PoslovnaBanka/PoslovnaBankaApplication.java
@@ -0,0 +1,13 @@
+package com.pi.PoslovnaBanka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoslovnaBankaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoslovnaBankaApplication.class, args);
+	}
+
+}
---
+++ PoslovnaBanka/src/test/java/com/pi/PoslovnaBanka/PoslovnaBankaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pi.PoslovnaBanka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoslovnaBankaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
