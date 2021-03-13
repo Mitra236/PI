b61d93942bfafa415532c6e8122e17187a2aa020
Mitra Pantic <mitra196506@gmail.com>
2021-03-13 14:20:10 +0100

    Transaction listing by debtor account

--- PoslovnaBanka/src/main/java/com/pi/PoslovnaBanka/repository/TransakcijaRepository.java
+++ PoslovnaBanka/src/main/java/com/pi/PoslovnaBanka/repository/TransakcijaRepository.java
@@ -10,9 +10,9 @@ import com.pi.PoslovnaBanka.entity.Transakcija;
 
 public interface TransakcijaRepository extends JpaRepository<Transakcija, Integer>{
 
-	@Query(value = "SELECT * FROM transakcije LEFT JOIN racuni_pravnih_lica ON transakcije.racun_duznika = racuni_pravnih_lica.broj_racuna LEFT JOIN klijenti ON racuni_pravnih_lica.klijent = klijenti.klijent_id WHERE racuni_pravnih_lica.broj_racuna = ? AND transakcije.datum_prijema BETWEEN ? AND ?", nativeQuery=true)
+	@Query(value = "SELECT * FROM transakcije LEFT JOIN racuni_pravnih_lica ON transakcije.racun_duznika = racuni_pravnih_lica.broj_racuna OR transakcije.racun_poverioca = racuni_pravnih_lica.broj_racuna LEFT JOIN klijenti ON racuni_pravnih_lica.klijent = klijenti.klijent_id WHERE racuni_pravnih_lica.broj_racuna = ? AND transakcije.datum_prijema BETWEEN ? AND ?", nativeQuery=true)
 	List<Transakcija> getTransactionByAccountNumberAndTimeRange(String accountNumber, Date fromDate, Date toDate);
 	
-	@Query(value = "SELECT * FROM transakcije LEFT JOIN racuni_pravnih_lica ON transakcije.racun_duznika = racuni_pravnih_lica.broj_racuna LEFT JOIN klijenti ON racuni_pravnih_lica.klijent = klijenti.klijent_id WHERE racuni_pravnih_lica.broj_racuna = ?", nativeQuery=true)
+	@Query(value = "SELECT * FROM transakcije LEFT JOIN racuni_pravnih_lica ON transakcije.racun_duznika = racuni_pravnih_lica.broj_racuna OR transakcije.racun_poverioca = racuni_pravnih_lica.broj_racuna LEFT JOIN klijenti ON racuni_pravnih_lica.klijent = klijenti.klijent_id WHERE racuni_pravnih_lica.broj_racuna = ?", nativeQuery=true)
 	List<Transakcija> getTransactionByAccountNumber(String accountNumber);
 }
